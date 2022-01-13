15e81a027cd39c6ef18911b634b4adb226db87f4
Elena Ghitescu <EGhitescu@neocasesoftware.com>
2022-01-13 18:54:08 +0100

    first version

---
+++ Exercices/RemoveDuplicates.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercices
+{
+    public class RemoveDuplicates
+    {
+        private const int lengthLimit = 100;
+
+        public static int[] Run(int[] source)
+        {
+            if (source == null || source.Length <= 1 )
+                return source;
+            if (source.Length > lengthLimit)
+                throw new ArgumentException("Source array is too long");
+            int firstValue = source[0];
+            BinaryTree<int> searchTree = new BinaryTree<int>(firstValue);
+            int length = source.Length;
+            bool[] duplicatedElements= new bool[length];
+            BinarySearch<int> binarySearch = new BinarySearch<int>();
+            for (int i = 1; i < length; i++)
+                duplicatedElements[i] = binarySearch.Search(source[i], searchTree);
+            
+            List<int> result = new List<int>(length);
+            for (int i = 0;i < length;i++)
+                if (!duplicatedElements[i])
+                    result.Add(source[i]);
+            return result.ToArray();
+        }
+
+
+    }
+
+    public class BinarySearch<T> where T : IComparable
+    {
+        public bool Search(T value, BinaryTree<T> binaryTree)
+        {
+            if ((value == null) || (binaryTree == null) || binaryTree.Value == null)
+                return false;
+            if (binaryTree.Value.Equals(value))
+                return true;
+            if (value.CompareTo(binaryTree.Value) < 0)
+                if (binaryTree.Left is null)
+                { 
+                    binaryTree.Left = new BinaryTree<T>(value);
+                    return false;
+                }
+                else
+                    return Search(value, binaryTree.Left);
+            if (value.CompareTo(binaryTree.Value) > 0)
+                if (binaryTree.Right is null)
+                {
+                    binaryTree.Right = new BinaryTree<T>(value);
+                    return false;
+                }
+                else
+                    return Search(value, binaryTree.Right);
+            //default case: should not happen
+            return false;
+        }
+    }
+
+    public class BinaryTree<T> where T : IComparable
+    {
+        public BinaryTree(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; private set; }
+        public BinaryTree<T> Left { get; set; }
+        public BinaryTree<T> Right { get; set; }
+    }
+}
---
+++ ExercicesTests/RemoveDuplicatesTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using Exercices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercices.Tests
+{
+    public class RemoveDuplicatesTests
+    {
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData(new int[]{}, new int[] {})]
+        [InlineData(new int[]{1}, new int[] {1})]
+        [InlineData(new int[] { 1, 1 }, new int[] { 1 })]
+        [InlineData(new int[] { 1, 2 }, new int[] { 1, 2 })]
+        [InlineData(new int[] { 7, 4, 7, 6, 12, 1, 4, -6}, new int[] { 7, 4, 6, 12, 1, -6 })]
+        [InlineData(new int[] { 1245 , 1245, 1245, 1245, 1245, 1245, 1245, 1245, 1247}, new int[] { 1245, 1247 })]
+        public void RunTest(int[] source, int[] result)
+        {
+            //act
+            var actualResult = RemoveDuplicates.Run(source);
+            //assert
+            if (source == null)
+            {
+                Assert.True(actualResult is null);
+                return;
+            }    
+            Assert.Equal(result.Length, actualResult.Length);
+            for(int i=0; i<result.Length; i++)
+                Assert.Equal(result[i], actualResult[i]);
+        }
+
+        [Fact]
+        public void Run_When_OversizedSource_Then_ExceptionIsThrown()
+        {
+            var sourceBuilder = new List<int>();
+            for (int i=0; i<101; i++)
+                sourceBuilder.Add(i);
+            var source = sourceBuilder.ToArray();
+            var ex = Assert.Throws<ArgumentException>(() => RemoveDuplicates.Run(source));
+            Assert.Equal("Source array is too long", ex.Message);
+        }
+    }
+}
